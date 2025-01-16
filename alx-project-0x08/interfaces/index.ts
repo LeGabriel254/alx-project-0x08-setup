ee36a34ab584551b4d26bf90ab2d11518db62bce
Leon Gabriel <g73346920@gmail.com>
2025-01-16 15:04:40 +0300

    Chored:ensure type safety 4 components that render child element.

--- alx-project-0x08/interfaces/index.ts
+++ alx-project-0x08/interfaces/index.ts
@@ -1,7 +1,18 @@
 import * as React from "react";
+import { ReactNode } from "react";
 
 // Define a common interface for React components that accept children
 // This ensures type safety for components that render child elements
 export interface ReactComponentProps {
-  children: React.ReactNode; // Represents any valid React node (elements, strings, fragments, etc.)
+  children:ReactNode; 
 }
+
+export interface GeneratedImageProps{
+  imageUrl:string
+  prompt:string
+  width?:string
+  height?:string
+  action: (imagePath: string) => void
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
